aed37c405153358e118b2e63b6fbf064be51a310
Guilherme Paes <guilhermep98@gmail.com>
2021-04-24 21:25:03 -0300

    Setup DB working

--- const.ts
+++ const.ts
@@ -3,3 +3,9 @@ export const ANSI_COLORS = {
     default: '\x1b[0m',
     blue: "\x1b[34m"
 }
+
+export const APP_COMPONENTS = {
+    SETUP_DB: "Setup DB"
+}
+
+export const BASE_MIGRATION_PATH = "database/migrations/sqls"
--- database/migrations/setup-db.ts
+++ database/migrations/setup-db.ts
@@ -1,17 +1,23 @@
 import * as fs from "fs";
 import { query } from "../connection";
-import Logger  from "../../utils/Logger";
+import Logger from "../../utils/Logger";
+import { APP_COMPONENTS, BASE_MIGRATION_PATH } from "../../const";
 
-/*let filehandle = fs.readFileSync("database/migrations/sqls/001-createPropertyType.sql", (err, data) =>{
-    console.log('data', data)
-});
-console.log(filehandle.toString())
-
-query(filehandle.toString(), [], (err, result) => {
-    if(err) throw err
-    if(result) console.log('Table is up')
-})*/
-
-const dir = fs.readdirSync("database/migrations/sqls");
-Logger.error("Migration", "Error")
-Logger.info('Migration', "info message...")
+try {
+  const dir = fs.readdirSync(BASE_MIGRATION_PATH);
+  Logger.info(
+    APP_COMPONENTS.SETUP_DB,
+    `Found ${dir.length} migration file(s)...`
+  );
+  for (let file of dir) {
+    Logger.info(APP_COMPONENTS.SETUP_DB, `Migrating ${file}...`);
+    let sqlFile = fs.readFileSync(`${BASE_MIGRATION_PATH}/${file}`);
+    query(sqlFile.toString(), [], (err, result) => {
+      if (err) throw err;
+      if (result)
+        Logger.info(APP_COMPONENTS.SETUP_DB, `Successfully migrated ${file}..`);
+    });
+  }
+} catch (error) {
+  Logger.error(APP_COMPONENTS.SETUP_DB, error);
+}
