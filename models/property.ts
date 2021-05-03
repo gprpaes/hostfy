a83036515d09e1c07f48e8a1fe7dc63589ea232e
Guilherme Paes <guilhermep98@gmail.com>
2021-05-03 01:08:19 -0300

    add property

---
+++ models/property.ts
@@ -0,0 +1,48 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default class Property {
+  constructor(
+    private id: string,
+    private name: string,
+    private propertyTypeId: string,
+    private address: string,
+    private roomAmount: number
+  ) {}
+
+  public getRoomAmount(): number {
+    return this.roomAmount;
+  }
+  public setRoomAmount(value: number) {
+    this.roomAmount = value;
+  }
+  public getAddress(): string {
+    return this.address;
+  }
+  public setAddress(value: string) {
+    this.address = value;
+  }
+
+  public setId(): void {
+    this.id = uuidv4();
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setPropertyTypeId(propertyTypeId: string): void {
+    this.propertyTypeId = propertyTypeId;
+  }
+
+  public getPropertyId(): string {
+    return this.propertyTypeId;
+  }
+}
