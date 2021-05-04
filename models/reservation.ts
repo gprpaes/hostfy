1844f399bd491b73ef5521e582d982b90f33c3e0
Guilherme Paes <guilhermep98@gmail.com>
2021-05-04 03:10:46 -0300

    Added reservation

---
+++ models/reservation.ts
@@ -0,0 +1,70 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default class Reservation {
+  public getCheckOut(): boolean {
+    return this.checkOut;
+  }
+  public setCheckOut(value: boolean) {
+    this.checkOut = value;
+  }
+  public getCheckIn(): boolean {
+    return this.checkIn;
+  }
+  public setCheckIn(value: boolean) {
+    this.checkIn = value;
+  }
+  public getReservationDate(): number {
+    return this.reservationDate;
+  }
+  public setReservationDate(value: number) {
+    this.reservationDate = value;
+  }
+  public getTotalValue(): number {
+    return this.totalValue;
+  }
+  public setTotalValue(value: number) {
+    this.totalValue = value;
+  }
+  public getGuestAmount(): number {
+    return this.guestAmount;
+  }
+  public setGuestAmount(value: number) {
+    this.guestAmount = value;
+  }
+  public getEndDate(): number {
+    return this.endDate;
+  }
+  public setEndDate(value: number) {
+    this.endDate = value;
+  }
+  public getStartDate(): number {
+    return this.startDate;
+  }
+  public setStartDate(value: number) {
+    this.startDate = value;
+  }
+  public getBedroomId(): string {
+    return this.bedroomId;
+  }
+  public setBedroomId(value: string) {
+    this.bedroomId = value;
+  }
+  public getId(): string {
+    return this.id;
+  }
+  public setId(value?: string) {
+    if (!value) this.id = uuidv4();
+    else this.id = value;
+  }
+  constructor(
+    private id: string,
+    private bedroomId: string,
+    private guestAmount: number,
+    private startDate: number,
+    private endDate: number,
+    private totalValue: number,
+    private reservationDate: number,
+    private checkIn: boolean,
+    private checkOut: boolean
+  ) {}
+}
