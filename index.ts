32bfc789d70484e44cc8f4b13f962ea1660458e5
gabrieloli21 <gabrieloliveira62@yahoo.com>
2021-05-05 20:35:05 -0300

    add cors

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import userRouter from "./routes/userRouter";
 import bp from "body-parser";
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
 
 app.use(bp.json());
+app.use(cors());
 app.use(bp.urlencoded({ extended: true }));
 app.use(userRouter);
 
