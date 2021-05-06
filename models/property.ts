37b2953bbe57991ba96b27f566f95091a9f9295b
Guilherme Paes <guilhermep98@gmail.com>
2021-05-05 22:39:40 -0300

    Added post

--- models/property.ts
+++ models/property.ts
@@ -2,12 +2,20 @@ import { v4 as uuidv4 } from "uuid";
 
 export default class Property {
   constructor(
-    private id: string,
-    private name: string,
-    private propertyTypeId: string,
-    private address: string,
-    private roomAmount: number
-  ) {}
+    private id: string | null,
+    private name: string | null,
+    private propertyTypeId: string | null,
+    private address: string | null,
+    private roomAmount: number | null,
+    private cnpj: string | null
+  ) {
+    this.setId();
+    this.setName(name);
+    this.setRoomAmount(roomAmount);
+    this.setAddress(address);
+    this.setPropertyTypeId(propertyTypeId)
+    this.setCnpj(cnpj)
+  }
 
   public getRoomAmount(): number {
     return this.roomAmount;
@@ -20,6 +28,13 @@ export default class Property {
   }
   public setAddress(value: string) {
     this.address = value;
+  }
+
+  public getCnpj(): string {
+    return this.cnpj;
+  }
+  public setCnpj(value: string) {
+    this.cnpj = value;
   }
 
   public setId(id?: string): void {
--- routes/propertyRouter.ts
+++ routes/propertyRouter.ts
@@ -1,48 +1,81 @@
-import express from "express"
-import Logger from "../utils/Logger"
-import {REST_RESOURCES, APP_COMPONENTS} from "../const"
-import {query} from "../database/connection"
+import express from "express";
+import Logger from "../utils/Logger";
+import { REST_RESOURCES, APP_COMPONENTS } from "../const";
+import { query } from "../database/connection";
+import Property from "../models/property";
 const propertyRouter = express.Router();
 
-
-
 propertyRouter.get(`${REST_RESOURCES.PROPERTY}/:id`, async (req, res) => {
-    const { id } = req.params;
-    Logger.info(APP_COMPONENTS.ENDPOINT, "Listing a PROPERTY given an id...");
-    try { 
-      const resq = await query(`SELECT * FROM property WHERE id = $1`, [id]);
-      return res.json({
-        success: true,
-        data: resq.rows,
-      });
-    } catch (error) {
-      res.status(500);
-      return res.json({
-        error: error.toString(),
-      });
-    }
-  });
+  const { id } = req.params;
+  Logger.info(APP_COMPONENTS.ENDPOINT, "Listing a PROPERTY given an id...");
+  try {
+    const resq = await query(`SELECT * FROM property WHERE id = $1`, [id]);
+    return res.json({
+      success: true,
+      data: resq.rows,
+    });
+  } catch (error) {
+    res.status(500);
+    return res.json({
+      error: error.toString(),
+    });
+  }
+});
 
-
-propertyRouter.get(`${REST_RESOURCES.PROPERTY}`, async(req, res) =>{
-    Logger.info(APP_COMPONENTS.ENDPOINT, "Listing all properties");
-    try{
-        const resq = await query("SELECT * FROM property", []);
+propertyRouter.get(`${REST_RESOURCES.PROPERTY}`, async (req, res) => {
+  Logger.info(APP_COMPONENTS.ENDPOINT, "Listing all properties");
+  try {
+    const resq = await query("SELECT * FROM property", []);
     Logger.success(APP_COMPONENTS.ENDPOINT, "Success listing property");
     res.status(200);
     return res.json({
       success: true,
       data: resq.rows,
     });
-    }catch(error){
-        res.status(500);
-        return res.json({
-          error: error.toString(),
-        });
-    }
-})
+  } catch (error) {
+    res.status(500);
+    return res.json({
+      error: error.toString(),
+    });
+  }
+});
 
-
+propertyRouter.post(`${REST_RESOURCES.PROPERTY}`, async (req, res) => {
+  Logger.info(APP_COMPONENTS.ENDPOINT, "Creating property....");
+  const { name, cnpj, address, roomAmount, propertyTypeId } = req.body || null;
+  const property = new Property(
+    null,
+    name || null,
+    propertyTypeId || null,
+    address || null,
+    roomAmount || null,
+    cnpj || null
+  );
+  try {
+    const resq = await query(
+      "INSERT INTO property VALUES($1,$2, $3, $4,$5,$6) RETURNING *",
+      [
+        property.getId(),
+        property.getName(),
+        property.getCnpj(),
+        property.getAddress(),
+        property.getRoomAmount(),
+        property.getPropertyId(),
+      ]
+    );
+    Logger.success(APP_COMPONENTS.ENDPOINT, "created a new property!");
+    res.status(201);
+    return res.json({
+      success: true,
+      data: resq.rows[0],
+    });
+  } catch (error) {
+    Logger.info(APP_COMPONENTS.ENDPOINT, "error on creating property");
+    res.status(400);
+    return res.json({
+      error: error.toString(),
+    });
+  }
+});
 
 export default propertyRouter;
-
