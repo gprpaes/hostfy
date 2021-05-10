8f21ada46abf00e9857f3a04c070d547450755bb
Guilherme Paes <guilhermep98@gmail.com>
2021-05-10 05:03:55 -0300

    minor changes

--- database/migrations/sqls/012-createReservation.sql
+++ database/migrations/sqls/012-createReservation.sql
@@ -3,10 +3,10 @@ CREATE TABLE IF NOT EXISTS reservation(
       id uuid PRIMARY KEY,
       bedroom_id uuid,
       guest_amount integer,
-      start_date bigint,
-      end_date bigint,
+      start_date text,
+      end_date text,
       total_value double precision,
-      reservation_date bigint,
+      reservation_date text,
       check_in boolean,
       CONSTRAINT fk_bedroom_id
       FOREIGN KEY(bedroom_id) 
